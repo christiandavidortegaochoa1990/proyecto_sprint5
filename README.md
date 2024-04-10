a96ec5e1fac80609a2815ae90f1df3fce87e8b8f
christiandavidortegaochoa1990 <c_ortega8a@hotmail.com>
2024-04-09 19:48:02 -0500

    Actualización de la información solicitada en el proyecto

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # proyecto_sprint5
-Repositorio creado para desarrollar el proyecto del sprint 5 en Tripleten
+Repositorio creado para desarrollar el proyecto del sprint 5 en Tripleten:
+
+Enlaces asociados:
+- Render: https://proyecto-sprint5-rjf2.onrender.com/
+- Repositorio Github: https://github.com/christiandavidortegaochoa1990/proyecto_sprint5
--- app.py
+++ app.py
@@ -2,16 +2,18 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+#Lectura de archivo 'vehicles_us.csv'
 car_data = pd.read_csv('vehicles_us.csv')
 
-st.header('Histograma y dispersión')
+#Creación de header
+st.header('Ventas de Coches - Análisis de datos visuales')
 
 
-hist_button = st.checkbox('Construir histograma') # crear un botón
+hist_button = st.checkbox('Presiona aquí para construir un histograma de los datos') # crear un botón
         
 if hist_button: # al hacer clic en el botón
             # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Visualización de histograma para el conjunto de datos de anuncios de venta de coches')
             
             # crear un histograma
     fig = px.histogram(car_data, x="odometer")
@@ -20,11 +22,11 @@ if hist_button: # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
 
 
-scatter_button = st.checkbox('Construir un diagrama de dispersión')
+scatter_button = st.checkbox('Presiona aquí para construir un diagrama de dispersión de los datos')
 
 if scatter_button: # al hacer clic en el botón
             # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Visualización de diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
             
             # crear un histograma
     fig = px.scatter(car_data, x="odometer", y="price")
